f3121b2b935b4517867ee40421b2fc55ff27e8ca
ledah22 <45542954+ledah22@users.noreply.github.com>
2022-01-10 22:31:27 +0000

    Phase plot improved
    added Hilbert transform for result comparison

--- phaseplot.m
+++ phaseplot.m
@@ -1,9 +1,11 @@
 function [] = phaseplot(thetas, Ts, input_data)
 [l, num_rhythms] = size(thetas);
+input_data_analytical = hilbert(input_data);
+input_data_phase = angle(input_data_analytical);
+
 figure
 for j = 1:num_rhythms
     subplot(num_rhythms, 1, j);
-    %plot(1:1:1000, thetas(1:1000, j));
     plot(1:1:l, thetas(:, j));
 end
 title("Phase tracked at sampling time of "+ 1/Ts + "Hz")
@@ -11,4 +13,6 @@ xlabel('Time (num. of samples)')
 ylabel('Amplitude (Radians and Volts)')
 hold on
 plot(input_data)
-legend('Phase', 'Input signal')
+hold on
+plot(input_data_phase)
+legend('Phase', 'Input signal', 'Hilbert Transform')
