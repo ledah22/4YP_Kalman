c62ae289a32b64f4f624ac7010f299e9bc8afaea
ledah22 <45542954+ledah22@users.noreply.github.com>
2022-01-10 14:47:29 +0000

    Axis on plots

--- phaseplot.m
+++ phaseplot.m
@@ -1,4 +1,4 @@
-function [] = phaseplot(thetas)
+function [] = phaseplot(thetas, Ts, input_data)
 [l, num_rhythms] = size(thetas);
 figure
 for j = 1:num_rhythms
@@ -6,3 +6,9 @@ for j = 1:num_rhythms
     %plot(1:1:1000, thetas(1:1000, j));
     plot(1:1:l, thetas(:, j));
 end
+title("Phase tracked at sampling time of "+ 1/Ts + "Hz")
+xlabel('Time (num. of samples)')
+ylabel('Amplitude (Radians and Volts)')
+hold on
+plot(input_data)
+legend('Phase', 'Input signal')
